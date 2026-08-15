728298a8096eb88b46894f727bdef4381ff1b51f
wjc129 <117416681+wjc129@users.noreply.github.com>
2026-08-15 11:25:39 +0800

    "修复Windows云构建编码"

--- make_icon.py
+++ make_icon.py
@@ -26,4 +26,4 @@ portrait.save(
     format="ICO",
     sizes=[(16, 16), (24, 24), (32, 32), (48, 48), (64, 64), (128, 128), (256, 256)],
 )
-print(root / "奶娃.ico")
+print("Application icons generated.")
